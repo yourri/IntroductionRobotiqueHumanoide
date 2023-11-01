7f498761c3b392d985339805b5ff488d814c3642
Thierry Leclaire <lect2803@gegi-lab3125-03.gegi.usherbrooke.ca>
2023-11-01 19:38:57 -0400

    Ca fonctionne dans SetupBipedRobot.m, criss (de mar)

--- Matlab/SetupBipedRobot.m
+++ Matlab/SetupBipedRobot.m
@@ -12,19 +12,19 @@ UZ = [0 0 1]';
 %%% a : Axe de rotation b : position selon le parent q: valeur du joint
 uLINK    = struct('name','MP_BODY'    , 'm', 0.97559947, 'sister', 0, 'child', 2, 'b',[0  0  0.2195]','a',UZ,'q',0);
 
-uLINK(2) = struct('name','MP_PELVIS_L' , 'm',  0.167, 'sister', 8, 'child', 3, 'b',[-0.005 0.037 -0.09355]'   ,'a',-UZ,'q',0);
-uLINK(3) = struct('name','MP_THIGH1_L' , 'm',  0.11904336, 'sister', 0, 'child', 4, 'b',[0 0 0.028652]'   ,'a',-UX,'q',0);
-uLINK(4) = struct('name','MP_THIGH2_L' , 'm',  0.11904336, 'sister', 0, 'child', 5, 'b',[0 0 0]'   ,'a',UY,'q',0);
-uLINK(5) = struct('name','MP_TIBIA_L' , 'm',  0.070309794, 'sister', 0, 'child', 6, 'b',[-0.093 0 0]' ,'a',UY,'q',0);
-uLINK(6) = struct('name','MP_ANKLE1_L' , 'm',  0.16710792, 'sister', 0, 'child', 7, 'b',[-0.093 0 0]' ,'a',-UY,'q',0);
-uLINK(7) = struct('name','MP_ANKLE2_L' , 'm',  0.0794462, 'sister', 0, 'child', 0, 'b',[0  0   0]' ,'a',UX,'q',0);
+uLINK(2) = struct('name','MP_PELVIS_L' , 'm',  0.167, 'sister', 8, 'child', 3, 'b',[-0.005 0.037 -0.09355]'   ,'a',UZ,'q',0);
+uLINK(3) = struct('name','MP_THIGH1_L' , 'm',  0.11904336, 'sister', 0, 'child', 4, 'b',[0 0 -0.028652]'   ,'a',UZ,'q',0);
+uLINK(4) = struct('name','MP_THIGH2_L' , 'm',  0.11904336, 'sister', 0, 'child', 5, 'b',[0 0 0]','a',UZ,'q',0);
+uLINK(5) = struct('name','MP_TIBIA_L' , 'm',  0.070309794, 'sister', 0, 'child', 6, 'b',[0 0 -0.093]' ,'a',UZ,'q',0);
+uLINK(6) = struct('name','MP_ANKLE1_L' , 'm',  0.16710792, 'sister', 0, 'child', 7, 'b',[ 0 0 -0.093]' ,'a',UZ,'q',0);
+uLINK(7) = struct('name','MP_ANKLE2_L' , 'm',  0.0794462, 'sister', 0, 'child', 0, 'b',[0  0   0]' ,'a',UZ,'q',0);
 
 uLINK(8) = struct('name','MP_PELVIS_R' , 'm',  0.197, 'sister', 0, 'child', 9, 'b',[-0.005 -0.037 -0.09355]'   ,'a',UZ,'q',0);
-uLINK(9) = struct('name','MP_THIGH1_R' , 'm',  0.11904336, 'sister', 0, 'child',10, 'b',[0 0 0.028652]'   ,'a',UX,'q',0);
-uLINK(10)= struct('name','MP_THIGH2_R' , 'm',  0.11904336, 'sister', 0, 'child',11, 'b',[0 0 0]'   ,'a',UY,'q',0);
-uLINK(11)= struct('name','MP_TIBIA_R' , 'm',  0.070309794, 'sister', 0, 'child',12, 'b',[-0.093 0 0]' ,'a',UY,'q',0);
-uLINK(12)= struct('name','MP_ANKLE1_R' , 'm',  0.16710792, 'sister', 0, 'child',13, 'b',[-0.093 0 0]' ,'a',UY,'q',0);
-uLINK(13)= struct('name','MP_ANKLE2_R' , 'm',  0.0794462, 'sister', 0, 'child', 0, 'b',[0  0   0]' ,'a',UX,'q',0);
+uLINK(9) = struct('name','MP_THIGH1_R' , 'm',  0.11904336, 'sister', 0, 'child',10, 'b',[0 0 -0.028652]'   ,'a',UZ,'q',0);
+uLINK(10)= struct('name','MP_THIGH2_R' , 'm',  0.11904336, 'sister', 0, 'child',11, 'b',[0 0 0]'   ,'a',UZ,'q',0);
+uLINK(11)= struct('name','MP_TIBIA_R' , 'm',  0.070309794, 'sister', 0, 'child',12, 'b',[ 0 0 -0.093]' ,'a',UZ,'q',0);
+uLINK(12)= struct('name','MP_ANKLE1_R' , 'm',  0.16710792, 'sister', 0, 'child',13, 'b',[ 0 0 -0.093]' ,'a',UZ,'q',0);
+uLINK(13)= struct('name','MP_ANKLE2_R' , 'm',  0.0794462, 'sister', 0, 'child', 0, 'b',[0  0   0]' ,'a',UZ,'q',0);
 
 [uLINK(1).vertex,uLINK(1).face]   = MakeBox([0.1 0.3 0.5]  ,[0.05 0.15 -0.05] );    % BODY
 [uLINK(7).vertex,uLINK(7).face]   = MakeBox([0.2 0.1 0.02] ,[0.05  0.05 0.05]);     % Foot
@@ -37,7 +37,7 @@ for n=1:length(uLINK)
     eval([uLINK(n).name,'=',num2str(n),';']);
 end
 
-uLINK(MP_BODY).p = [0.0, 0.0, 0.65]';
+uLINK(MP_BODY).p = [0.0, 0.0, 0.214625 + 0.143577]';
 uLINK(MP_BODY).R = eye(3);
 ForwardKinematics(1);
 
