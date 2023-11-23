4342a7b398660a6da7ce03b7c03bba0ff9ef97b0
Julien Lavoie-Bernier <lavj3014@usherbrooke.ca>
2023-11-22 20:39:30 -0500

    lol wip

--- Matlab/team_A_forward_kinematics.m
+++ Matlab/team_A_forward_kinematics.m
@@ -57,3 +57,48 @@ while 1
     fprintf('Type any key for another pose, Ctrl-C to abort\n');
     pause
 end
+
+
+
+
+function [trajectoire_pied_x] =  creation_trajectoire_pied_x(Xi, Xf, Ti, Tf)
+    Vix=0;
+    Vfx=0;
+
+
+    ax=Vix;
+    cx=(-2*(Xf-Xi))*Tf^3;
+    bx=(-3*cx/2)*Tf;
+    dx=Xi;
+
+    t=[Ti:Ts:Tf]';
+
+    trajectoire_pied_x = cx*t.^3 + bx*t.*t + ax*t + dx;
+end
+
+function [trajectoire_pied_y] =  creation_trajectoire_pied_y(Yi, Yf, Ti, Tf)
+    Viy=0;
+    Vfy=0;
+
+    ay=Viy;
+    cy=(-2*(Yf-Yi))*Tf^3;
+    by=(-3*cy/2)*Tf;
+    dy=Yi;
+
+
+    trajectoire_pied_y = cy*t.^3 + by*t.*t + ay*t + dy;
+end
+
+function [trajectoire_pied_Z] = creation_trajectoire_pied_z(Zi, Zf, Ti, Tf)
+    deltaZ=Zf-Zi;
+    zmax  =0.02;
+
+    Viz   =0;
+    Vfz   =0; %mètres par seconde
+
+    syms a b c d e
+    eqns = [a == Zi, b == Viz, a+b*Tf+c*Tf^2+d*Tf^3+e*Tf^4 == Zf, a+b*Tf/2+c*(Tf/2)^2+d*(Tf/2)^3+e*(Tf/2)^4 == zmax, b+2*c*Tf+3*d*Tf^2+4*e*Tf^3 == 0]; 
+    [az, bz, cz, dz, ez] = solve(eqns, [a b c d e]);
+
+    trajectoire_pied_Z = ez*t.^4 + dz*t.^3 + cz*t.*t + bz*t + az;
+end
--- Matlab/team_A_inverse_kinematics.m
+++ Matlab/team_A_inverse_kinematics.m
@@ -3,17 +3,16 @@
 % 2004 Dec.17 s.kajita AIST
 
 close all
-clear              % claer work space
+clear              % clear work space
 global uLINK   
-global Trajx
-global Trajy
-global Trajz
-global zmprefx
-global zmprefy
 global temps_un_pas 
 global Ts
+global comx
+global comy
+global dX
 
-Ts = 0.01
+Ts = 0.05;
+Temps_initial = 2;
 teamA_setup_darwin; 
 team_A_ZMP_trajectory;
 % Set the biped robot of Fig.2.19 and Fig.2.20
@@ -45,32 +44,94 @@ ForwardKinematics(1);
 
 p_ankle_r_int = uLINK(MP_ANKLE2_R).p;
 p_ankle_l_int = uLINK(MP_ANKLE2_L).p;
+p_body = uLINK(MP_BODY).p;
 
-clf
-    DrawAllJoints(1);
-    view(38,10)
-    axis equal
-    zlim([-0.2 1.2])
-    grid on
-for i=1:length(temps_un_pas)
-%     uLINK(MP_BODY).p = [0.0, 0.0, 0.5]';
-%     uLINK(MP_BODY).R = eye(3);
-%     
-    
-%     Rfoot.p = p_ankle_r_int + [Trajx(i) Trajy(i) Trajz(i)]';
-%     Rfoot.R = RPY2R(1/2*pi*(rand(3,1)-0.5));  %  -pi/4 < q < pi/4
-%     rerr_norm = InverseKinematics(MP_ANKLE2_R, Rfoot);
-%     
-%     Lfoot.p = p_ankle_l_int + [Trajx(i) Trajy(i) Trajz(i)]';
-%     Lfoot.R = RPY2R(1/2*pi*(rand(3,1)-0.5)); %  -pi/4 < q < pi/4
-%     lerr_norm = InverseKinematics(MP_ANKLE2_L, Lfoot);
+ 
+for i=1:7
+    if mod(i, 2) == 1 
+        % Trajectoire pour le pied gauche
+        trajectoire_x = creation_trajectoire_pied_x(p_ankle_l_int(1), p_ankle_l_int(1) + dX, 0, temps_un_pas, Ts);
+        trajectoire_y = creation_trajectoire_pied_y(p_ankle_l_int(2), p_ankle_l_int(2), 0, temps_un_pas, Ts);
+        trajectoire_z = creation_trajectoire_pied_z(p_ankle_l_int(3), p_ankle_l_int(3), 0, temps_un_pas, Ts);
+    else
+        % Trajectoire pour le pied droit
+        trajectoire_x = creation_trajectoire_pied_x(p_ankle_r_int(1), p_ankle_r_int(1) + dX*2, 0, temps_un_pas, Ts);
+        trajectoire_y = creation_trajectoire_pied_y(p_ankle_r_int(2), p_ankle_r_int(2), 0, temps_un_pas, Ts);
+        trajectoire_z = creation_trajectoire_pied_z(p_ankle_r_int(3), p_ankle_r_int(3), 0, temps_un_pas, Ts);
+    end
+
+    for j=1+((i-1) * length(trajectoire_x)):1+((i-1) * length(trajectoire_x)) + length(trajectoire_x)
+        index = j - (i-1) * length(trajectoire_x);
+        if mod(i,2) == 1
+            Lfoot.p = p_ankle_l_int + [trajectoire_x(index) trajectoire_y(index) trajectoire_z(index)']';
+            Lfoot.R = RPY2R(1/2*pi*[0, 0, 0]);  %  -pi/4 < q < pi/4
+            rerr_norm1 = InverseKinematics(MP_ANKLE2_L, Lfoot);     
+        else
+            Rfoot.p = p_ankle_r_int + [trajectoire_x(index) trajectoire_y(index) trajectoire_z(index)']';
+            Rfoot.R = RPY2R(1/2*pi*[0, 0, 0]); 
+            rerr_norm1 = InverseKinematics(MP_ANKLE2_R, Rfoot);
+        end
+        uLINK(MP_BODY).p = p_body + [comx(j)+0.015 comy(j) 0]';
         
-    clf
-    DrawAllJoints(1);
-    view(38,10)
-    axis equal
-    zlim([-0.2 1.2])
-    grid on
-    
-    java.lang.Trhread.sleep(10)
+        clf
+        DrawAllJoints(1);
+        view(38,10)
+        axis equal
+        zlim([-0.2 1.2])
+        grid on
+
+        fprintf('Type any key for another pose, Ctrl-C to abort\n');
+        pause
+    end   
+end
+
+
+function [trajectoire_pied_x] =  creation_trajectoire_pied_x(Xi, Xf, Ti, Tf, Ts)
+    Vix=0;
+    Vfx=0;
+
+
+    ax=Vix;
+    cx=(-2*(Xf-Xi))/Tf^3;
+    bx=(-3*cx/2)*Tf;
+    dx=Xi;
+
+    t=[Ti:Ts:Tf]';
+
+    trajectoire_pied_x = cx*t.^3 + bx*t.*t + ax*t + dx;
+end
+
+function [trajectoire_pied_y] =  creation_trajectoire_pied_y(Yi, Yf, Ti, Tf, Ts)
+    Viy=0;
+    Vfy=0;
+
+    ay=Viy;
+    cy=(-2*(Yf-Yi))/Tf^3;
+    by=(-3*cy/2)*Tf;
+    dy=Yi;
+
+    t=[Ti:Ts:Tf]';
+    trajectoire_pied_y = cy*t.^3 + by*t.*t + ay*t + dy;
+end
+
+function trajectoire_pied_Z = creation_trajectoire_pied_z(Zi, Zf, Ti, Tf, Ts)
+    deltaZ = Zf - Zi;
+    zmax = 0.02;
+
+    Viz = 0;
+    Vfz = 0; % mètres par seconde
+
+    t = Ti:Ts:Tf;
+
+    A = [1, Ti, Ti^2, Ti^3, Ti^4; 
+         0, 1, 2*Ti, 3*Ti^2, 4*Ti^3; 
+         1, Tf, Tf^2, Tf^3, Tf^4; 
+         0, 1, 2*Tf, 3*Tf^2, 4*Tf^3; 
+         0, 1/2, (Tf/2)^2, (Tf/2)^3, (Tf/2)^4];
+
+    b = [Zi; Viz; Zf; Vfz; zmax];
+
+    x = A\b;
+
+    trajectoire_pied_Z = x(5)*t.^4 + x(4)*t.^3 + x(3)*t.^2 + x(2)*t + x(1);
 end
